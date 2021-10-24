bd488bcc27eb602c9c31ccf72fca9c23b920e2a6
pan! <harvy@live.ru>
2021-10-24 23:07:43 +0500

    added merchandise rest controller

---
+++ src/MerchandiseService/Controllers/MerchendiseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerchandiseService.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public class MerchendiseController : ControllerBase
+    {
+        [Route("api/v1/merchandise/request")]
+        public IActionResult FetchRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("api/v1/merchandise/info")]
+        public IActionResult GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
