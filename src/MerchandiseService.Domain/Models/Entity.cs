a12b47a93601dbce30b93f6f31288dfb8161af36
pan! <harvy@webotdel.pro>
2021-11-04 13:46:43 +0500

    added hollow projects

---
+++ src/MerchandiseService.Domain/Models/Entity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+
+namespace MerchandiseService.Domain.Models
+{
+    public abstract class Entity
+    {
+        private int? _RequestedHashCode;
+
+        public virtual int Id { get; protected set; }
+
+        private List<INotification> _DomainEvents;
+        public IReadOnlyCollection<INotification> DomainEvents => this._DomainEvents?.AsReadOnly();
+
+        public void AddDomainEvent(INotification eventItem)
+        {
+            this._DomainEvents ??= new List<INotification>();
+            this._DomainEvents.Add(eventItem);
+        }
+
+        public void RemoveDomainEvent(INotification eventItem)
+        {
+            this._DomainEvents?.Remove(eventItem);
+        }
+
+        public void ClearDomainEvents()
+        {
+            this._DomainEvents?.Clear();
+        }
+
+        public bool IsTransient() => this.Id == default;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Entity entity)
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, entity))
+            {
+                return true;
+            }
+
+            if (this.GetType() != entity.GetType())
+            {
+                return false;
+            }
+
+            if (entity.IsTransient() || this.IsTransient())
+            {
+                return false;
+            }
+
+            return entity.Id == this.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            if (!this.IsTransient())
+            {
+                if (!this._RequestedHashCode.HasValue)
+                {
+                    this._RequestedHashCode = HashCode.Combine(Id, 31);
+                }
+
+                return _RequestedHashCode.Value;
+            }
+
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(Entity left, Entity right) =>
+            Object.Equals(left, null) ? Object.Equals(right, null) : left.Equals(right);
+
+        public static bool operator !=(Entity left, Entity right) => !(left == right);
+    }
+}
---
+++ src/MerchandiseService.Domain/Models/ValueObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MerchandiseService.Domain.Models
+{
+    public abstract class ValueObject
+    {
+        protected static bool EqualOperator(ValueObject left, ValueObject right)
+        {
+            if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return ReferenceEquals(left, null) || left.Equals(right);
+        }
+
+        protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+        {
+            return !EqualOperator(left, right);
+        }
+
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return this.GetEqualityComponents().SequenceEqual(((ValueObject)obj).GetEqualityComponents());
+        }
+
+        public override int GetHashCode() =>
+            GetEqualityComponents().Select(x => (x?.GetHashCode()) ?? 0).Aggregate((x, y) => x ^ y);
+
+        public ValueObject GetCopy() => this.MemberwiseClone() as ValueObject;
+    }
+}
