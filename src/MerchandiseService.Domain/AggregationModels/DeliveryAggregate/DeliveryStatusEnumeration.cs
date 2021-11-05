d797b6fd739b7823d4d0642f0d196ed2e7726672
pan! <harvy@webotdel.pro>
2021-11-05 05:59:54 +0500

    create object simplified

--- src/MerchandiseService.Domain/AggregationModels/DeliveryAggregate/DeliveryStatusEnumeration.cs
+++ src/MerchandiseService.Domain/AggregationModels/DeliveryAggregate/DeliveryStatusEnumeration.cs
@@ -4,8 +4,8 @@ namespace MerchandiseService.Domain.AggregationModels.DeliveryAggregate
 {
     public sealed class DeliveryStatusEnumeration : Enumeration
     {
-        public static readonly DeliveryStatusEnumeration InWork = new DeliveryStatusEnumeration(1, nameof(InWork));
-        public static readonly DeliveryStatusEnumeration Done = new DeliveryStatusEnumeration(2, nameof(Done));
+        public static readonly DeliveryStatusEnumeration InWork = new(1, nameof(InWork));
+        public static readonly DeliveryStatusEnumeration Done = new(2, nameof(Done));
 
         private DeliveryStatusEnumeration(int id, string name) : base(id, name)
         {
